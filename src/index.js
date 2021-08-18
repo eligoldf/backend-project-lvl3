97272b54e8048a6d82f7f1ae88aee956bb1c95f0
Eli <eli.goldfdr@gmail.com>
2021-08-18 17:57:46 +0300

    add pageloder function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import axios from 'axios';
-import * as fs from 'fs/promises';
+import { promises as fs } from 'fs';
+import path from 'path';
 import extractUrl from './utils';
+
+const pageLoader = (urlName, dirName = __dirname) => axios.get(urlName)
+  .then((response) => {
+    const fileName = extractUrl(urlName);
+    fs.writeFile(path.join(dirName, fileName), response.data);
+    return 'Done';
+  });
+
+export default pageLoader;
